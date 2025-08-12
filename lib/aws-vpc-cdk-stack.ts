9a235319b73e182957ecbd0432016cf296124b39
xiu-shi <eupirate@gmail.com>
2025-08-12 19:18:47 +0100

    Initial commit: CDK VPC project

---
+++ lib/aws-vpc-cdk-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsVpcCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, // 2 AZs
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+      });
+    // Outputs
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: vpc.vpcId,
+    });
+  }
+}
