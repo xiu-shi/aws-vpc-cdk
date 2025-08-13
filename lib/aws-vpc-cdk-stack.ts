43bdb41e6dedfd046fa129f80fb0706113f4b81c
xiu-shi <eupirate@gmail.com>
2025-08-13 16:34:24 +0100

    added EC2 1

--- lib/aws-vpc-cdk-stack.ts
+++ lib/aws-vpc-cdk-stack.ts
@@ -4,12 +4,14 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsVpcCdkStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+   
 
     // The code that defines your stack goes here
     // Level 2 constructs
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
+    this.vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2, // 2 AZs
       subnetConfiguration: [
         {
@@ -22,11 +24,16 @@ export class AwsVpcCdkStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
           cidrMask: 24,
         },
+        {
+          name: 'Database',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        }
       ],
       });
     // Outputs
     new cdk.CfnOutput(this, 'VpcId', {
-      value: vpc.vpcId,
+      value: this.vpc.vpcId,
     });
   }
 }
---
+++ lib/ec2-stack.ts
@@ -0,0 +1,30 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+// Props
+interface Ec2StackProps extends cdk.StackProps {
+  vpc: ec2.Vpc;
+}
+export class Ec2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: Ec2StackProps) {
+    super(scope, id, props);
+
+    // EC2 Instance
+    const instance = new ec2.Instance(this, "MyPrivateEC2", {
+        vpc: props.vpc,
+        vpcSubnets: {
+            subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+        machineImage: new ec2.AmazonLinuxImage({
+            generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+        }),
+        instanceType: ec2.InstanceType.of(
+            ec2.InstanceClass.T3,
+            ec2.InstanceSize.MICRO
+        ),
+    });
+         cdk.Tags.of(instance).add("Name", "MyPrivateEC2");
+  }
+}
